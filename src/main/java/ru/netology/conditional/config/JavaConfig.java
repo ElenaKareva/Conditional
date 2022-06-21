3fe6aa3dcbcc9371838fc5408ba7fac8ec863def
ElenaKareva <92863528+ElenaKareva@users.noreply.github.com>
2022-06-21 14:23:21 +0300

    added files

---
+++ src/main/java/ru/netology/conditional/config/JavaConfig.java
@@ -0,0 +1,25 @@
+package ru.netology.conditional.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.conditional.profiles.DevProfile;
+import ru.netology.conditional.profiles.ProductionProfile;
+import ru.netology.conditional.profiles.SystemProfile;
+
+@Configuration
+public class JavaConfig {
+
+    @Bean
+    @ConditionalOnProperty(value = "netology.profile.dev", matchIfMissing = true,
+            havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
---
+++ src/main/java/ru/netology/conditional/profiles/SystemProfile.java
@@ -0,0 +1,5 @@
+package ru.netology.conditional.profiles;
+
+public interface SystemProfile {
+    String getProfile();
+}
